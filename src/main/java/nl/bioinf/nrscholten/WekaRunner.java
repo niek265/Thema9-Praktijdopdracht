134575906e9c8ad10f82b8160965674235f4e360
niek265 <niek265@gmail.com>
2022-10-31 20:05:51 +0100

    General updates.

---
+++ src/main/java/nl/bioinf/nrscholten/WekaRunner.java
@@ -0,0 +1,10 @@
+package nl.bioinf.nrscholten;
+
+import weka.classifiers.trees.RandomForest;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class WekaRunner {
+
+}
